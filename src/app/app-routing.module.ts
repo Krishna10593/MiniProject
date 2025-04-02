2488794148f13066e9e0a3ce8d3eaff2f0142538
Krishna10593 <k.guru10593@gmail.com>
2025-04-02 22:16:38 +0530

    appFiles and homeFiles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { HomeComponent } from './home/home.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { ErrorComponent } from './error/error.component';
+import { AuthenticationGuard } from './authentication.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:LoginComponent},
+  {path:"dashboard", component:DashboardComponent, canActivate:[AuthenticationGuard], children:[
+    {path:"employees", component:EmployeesComponent},
+    {path:"home", component:HomeComponent},
+    {path: "create-employee", component:CreateEmployeeComponent},
+    {path:"employee-details/:id", component:EmployeeDetailsComponent},
+    {path:"edit-employee/:id", component:CreateEmployeeComponent}
+  ]},
+  {path:"login", component:LoginComponent},
+  {path:"**", component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { HomeComponent } from './home/home.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EditeployeeComponent } from './editeployee/editeployee.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    EmployeesComponent,
+    HomeComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EditeployeeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
